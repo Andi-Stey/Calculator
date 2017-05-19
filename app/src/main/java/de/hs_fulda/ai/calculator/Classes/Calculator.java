b60ea0f2b6ac97d5eda35f5ce98aabe1f6028599
Andi-Stey <andreas_stey@gmx.de>
2017-05-20 00:49:06 +0200

    Implemented multiply, divide, add, subtract methods.

--- app/src/main/java/de/hs_fulda/ai/calculator/Classes/Calculator.java
+++ app/src/main/java/de/hs_fulda/ai/calculator/Classes/Calculator.java
@@ -9,31 +9,54 @@ import de.hs_fulda.ai.calculator.Repository.IRepository;
 public class Calculator implements IRepository
 {
 
+    /***
+     * Multiply two numbers.
+     * @param number1
+     * @param number2
+     * @return double - the calculated value.
+     */
     @Override
     public double multiply(double number1, double number2)
     {
-        //TODO
-        return 0;
+        return number1 * number2;
     }
 
+
+    /***
+     * Divide number1 with number2.
+     * @param number1
+     * @param number2
+     * @return double - the calculated value.
+     */
     @Override
     public double divide(double number1, double number2)
     {
-        //TODO
-        return 0;
+        return number1 / number2;
     }
 
+
+    /***
+     * Add two numbers.
+     * @param number1
+     * @param number2
+     * @return double - the calculated value.
+     */
     @Override
     public double add(double number1, double number2)
     {
-        //TODO
-        return 0;
+        return number1 + number2;
     }
 
+
+    /***
+     * Subtract two numbers.
+     * @param number1
+     * @param number2
+     * @return double - the calculated value.
+     */
     @Override
     public double subtract(double number1, double number2)
     {
-        //TODO
-        return 0;
+        return number1 - number2;
     }
 }
