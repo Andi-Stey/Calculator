c76d68f3be01e400b04bd17d7cb5aedd7c999e6c
Andi-Stey <andreas_stey@gmx.de>
2017-05-20 00:20:34 +0200

    Calculator: added interface and the required methods.
    MainActivity:
    added interface and the required methods.

---
+++ app/src/main/java/de/hs_fulda/ai/calculator/Classes/Calculator.java
@@ -0,0 +1,31 @@
+package de.hs_fulda.ai.calculator.Classes;
+
+import de.hs_fulda.ai.calculator.Repository.IRepository;
+
+/**
+ * Created by edv-steynb on 20.05.2017.
+ */
+
+public class Calculator implements IRepository
+{
+
+    @Override
+    public void multiply(double number1, double number2) {
+
+    }
+
+    @Override
+    public void divide(double number1, double number2) {
+
+    }
+
+    @Override
+    public void add(double number1, double number2) {
+
+    }
+
+    @Override
+    public void subtract(double number1, double number2) {
+
+    }
+}
--- app/src/main/java/de/hs_fulda/ai/calculator/MainActivity.java
+++ app/src/main/java/de/hs_fulda/ai/calculator/MainActivity.java
@@ -3,11 +3,90 @@ package de.hs_fulda.ai.calculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import de.hs_fulda.ai.calculator.Repository.IOnClickRepository;
+
+public class MainActivity extends AppCompatActivity implements IOnClickRepository
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void calculate() {
+
+    }
+
+    @Override
+    public void zero() {
+
+    }
+
+    @Override
+    public void one() {
+
+    }
+
+    @Override
+    public void two() {
+
+    }
+
+    @Override
+    public void three() {
+
+    }
+
+    @Override
+    public void four() {
+
+    }
+
+    @Override
+    public void five() {
+
+    }
+
+    @Override
+    public void six() {
+
+    }
+
+    @Override
+    public void seven() {
+
+    }
+
+    @Override
+    public void eight() {
+
+    }
+
+    @Override
+    public void nine() {
+
+    }
+
+    @Override
+    public void dot() {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void allClear() {
+
+    }
+
+    @Override
+    public void clearLastDigit() {
+
     }
 }
