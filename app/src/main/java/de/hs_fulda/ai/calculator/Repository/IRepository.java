f4edede0c3e889a929c115bcc331917267ffe153
Andi-Stey <andreas_stey@gmx.de>
2017-05-19 21:27:54 +0200

    Added comments to IRepository methods.

--- app/src/main/java/de/hs_fulda/ai/calculator/Repository/IRepository.java
+++ app/src/main/java/de/hs_fulda/ai/calculator/Repository/IRepository.java
@@ -6,12 +6,36 @@ package de.hs_fulda.ai.calculator.Repository;
 
 public interface IRepository
 {
+
+    /***
+     * Multiply two numbers.
+     * @param number1
+     * @param number2
+     */
     void multiply(double number1, double number2);
 
+
+    /***
+     * Divide two numbers.
+     * @param number1
+     * @param number2
+     */
     void divide(double number1, double number2);
 
+
+    /***
+     * Add two numbers.
+     * @param number1
+     * @param number2
+     */
     void add(double number1, double number2);
 
+
+    /***
+     * Subtract two numbers.
+     * @param number1
+     * @param number2
+     */
     void subtract(double number1, double number2);
 
 }
