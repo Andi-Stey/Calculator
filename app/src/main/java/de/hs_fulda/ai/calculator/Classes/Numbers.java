a0e028f8ced1430881c9950b82f28de29e4533fd
Andi-Stey <andreas_stey@gmx.de>
2017-05-20 03:18:55 +0200

    Implemented Button-Listeners for numeric Buttons. Added Validation to Zero- and Dot-Button.

---
+++ app/src/main/java/de/hs_fulda/ai/calculator/Classes/Numbers.java
@@ -0,0 +1,21 @@
+package de.hs_fulda.ai.calculator.Classes;
+
+/**
+ * Created by edv-steynb on 20.05.2017.
+ */
+
+public final class Numbers
+{
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+    public static final int FOUR = 4;
+    public static final int FIVE = 5;
+    public static final int SIX = 6;
+    public static final int SEVEN = 7;
+    public static final int EIGHT = 8;
+    public static final int NINE = 9;
+    public static final char DOT = '.';
+
+}
--- app/src/main/java/de/hs_fulda/ai/calculator/MainActivity.java
+++ app/src/main/java/de/hs_fulda/ai/calculator/MainActivity.java
@@ -2,17 +2,37 @@ package de.hs_fulda.ai.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import de.hs_fulda.ai.calculator.Classes.Numbers;
 import de.hs_fulda.ai.calculator.Repository.IOnClickRepository;
 
 public class MainActivity extends AppCompatActivity implements IOnClickRepository
 {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.editInput) ;
+        editText.setShowSoftInputOnFocus(false);
+        zero();
+        one();
+        two();
+        three();
+        four();
+        five();
+        six();
+        seven();
+        eight();
+        nine();
+        dot();
     }
 
     @Override
@@ -24,67 +44,156 @@ public class MainActivity extends AppCompatActivity implements IOnClickRepositor
     @Override
     public void zero()
     {
-        //TODO
+        Button btnZero = (Button) findViewById(R.id.btnZero);
+        btnZero.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setTextOfEditText(Numbers.ZERO);
+            }
+        });
     }
 
     @Override
     public void one()
     {
-        //TODO
+        Button btnOne = (Button) findViewById(R.id.btnOne);
+        btnOne.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setTextOfEditText(Numbers.ONE);
+            }
+        });
     }
 
     @Override
     public void two()
     {
-        //TODO
+        Button btnTwo = (Button) findViewById(R.id.btnTwo);
+        btnTwo.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setTextOfEditText(Numbers.TWO);
+
+            }
+        });
     }
 
     @Override
     public void three()
     {
-        //TODO
+        Button btnThree = (Button) findViewById(R.id.btnThree);
+        btnThree.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setTextOfEditText(Numbers.THREE);
+            }
+        });
     }
 
     @Override
     public void four()
     {
-        //TODO
+        Button btnFour = (Button) findViewById(R.id.btnFour);
+        btnFour.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setTextOfEditText(Numbers.FOUR);
+            }
+        });
     }
 
     @Override
     public void five()
     {
-        //TODO
+        Button btnFive = (Button) findViewById(R.id.btnFive);
+        btnFive.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setTextOfEditText(Numbers.FIVE);
+            }
+        });
     }
 
     @Override
     public void six()
     {
-        //TODO
+        Button btnSix = (Button) findViewById(R.id.btnSix);
+        btnSix.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setTextOfEditText(Numbers.SIX);
+            }
+        });
     }
 
     @Override
     public void seven()
     {
-        //TODO
+        Button btnSeven = (Button) findViewById(R.id.btnSeven);
+        btnSeven.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setTextOfEditText(Numbers.SEVEN);
+            }
+        });
     }
 
     @Override
     public void eight()
     {
-        //TODO
+        Button btnEight = (Button) findViewById(R.id.btnEight);
+        btnEight.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setTextOfEditText(Numbers.EIGHT);
+            }
+        });
     }
 
     @Override
     public void nine()
     {
-        //TODO
+        Button btnNine = (Button) findViewById(R.id.btnNine);
+        btnNine.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setTextOfEditText(Numbers.NINE);
+            }
+        });
     }
 
     @Override
     public void dot()
     {
-        //TODO
+        Button btnDot = (Button) findViewById(R.id.btnDot);
+        btnDot.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setTextOfEditText(Numbers.DOT);
+            }
+        });
     }
 
     @Override
@@ -104,4 +213,41 @@ public class MainActivity extends AppCompatActivity implements IOnClickRepositor
     {
         //TODO
     }
+
+    public String getText()
+    {
+        TextView editInput = (TextView) findViewById(R.id.editInput);
+        String appendedNumber ;
+        appendedNumber = editInput.getText().toString();
+        return appendedNumber;
+    }
+
+    public void setTextOfEditText(Object number)
+    {
+        TextView editInput = (TextView) findViewById(R.id.editInput);
+
+        //only one dot is possible
+        if (number instanceof Character)
+        {
+            if (!getText().contains(String.valueOf(Numbers.DOT)))
+            {
+                editInput.setText(getText() + String.valueOf(number));
+            }
+        }
+        //numbers are always possible
+        else
+        {
+            //a 2 digits number can not start with 0
+            if(getText().length() == 1 && getText().equals(String.valueOf(Numbers.ZERO)))
+            {
+                editInput.setText(String.valueOf(number));
+            }
+            //every other input can have more than 1 digit.
+            else
+            {
+                editInput.setText(getText() + String.valueOf(number));
+            }
+        }
+    }
+
 }
